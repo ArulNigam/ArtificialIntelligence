eb3b3226093a35f032cba137ff022ed1bf5a77e4
Arul Nigam <14031728+ArulNigam@users.noreply.github.com>
2019-11-06 11:21:26 -0500

    problem w/ isValid

--- Unit2_ConstraintSatisfactionProblemTechniques/Nigam_A_Lab3.py
+++ Unit2_ConstraintSatisfactionProblemTechniques/Nigam_A_Lab3.py
@@ -4,7 +4,7 @@
 from tkinter import * 
 from graphics import *
 
-def check_complete(assignment, tri_to_hex_dict, possible_ints, hex_to_tri_dict):     
+def check_complete(assignment):     
    # Goal Test: are there any unassigned (".") 
    if assignment == None or assignment == []:
       return False      
@@ -13,94 +13,107 @@ def check_complete(assignment, tri_to_hex_dict, possible_ints, hex_to_tri_dict):
          return False
    return True
 
-def select_unassigned_var(assignment, tri_to_hex_dict, possible_ints, hex_to_tri_dict): 
+def select_unassigned_var(assignment): 
    # Select an unassigned variable - forward checking, MRV, or LCV
    # returns a variable
-   for key in range(24):
+   for key in range(81):
       if assignment[key] == ".": 
          return key
             
-def isValid(value, cell, assignment, tri_to_hex_dict, possible_ints, hex_to_tri_dict):
-   for hex in tri_to_hex_dict[cell]: 
-      for tri in hex_to_tri_dict[hex]:
-         if assignment[tri] != ".":
-            if int(assignment[tri]) == value and tri != cell: # neighbors are the same:
-               return False # invalid 
+def isValid(value, cell, assignment, cell_to_groups_dict, possible_ints):
+   #############################################################################################
+   #############################################################################################
+   #############################################################################################
+   #############################################################################################
+   #############################################################################################
+   #############################################################################################   
+   #ALWAYS ASSIGNS 1 FOR INDEX 0?????????????????????????!!!!!!!!!!!!!!!!!!!!###################
+   #############################################################################################
+   #############################################################################################
+   #############################################################################################
+   #############################################################################################
+   #############################################################################################
+   #############################################################################################
+   row_neighbors = []
+   col_neighbors = []
+   square_neighbors = []
+   for i in range(len(assignment)):
+      i = int(i)
+      if cell_to_groups_dict[cell][0] == int(i / 9): # same row   
+         row_neighbors.append(i)
+      if cell_to_groups_dict[cell][1] == i % 9: # same col   
+         col_neighbors.append(i)
+      if same_square(cell, i): # same sub-square
+         square_neighbors.append(i)
+   for i in cell_to_groups_dict[cell]: 
+      for a in row_neighbors:
+         if assignment[a] != ".":
+            if int(assignment[a]) == value and a != cell: # neighbors are the same:
+               return False # invalid
+      for b in col_neighbors:
+         if assignment[b] != ".":
+            if int(assignment[b]) == value and b != cell: # neighbors are the same:
+               return False # invalid
+      for c in square_neighbors:    
+            if assignment[c] != ".":
+               if int(assignment[c]) == value and c != cell: # neighbors are the same:
+                  return False # invalid
    return True
 
-def backtracking_search(assignment, cell_to_squares_dict, possible_ints, square_to_cells_dict):    
-   return recursive_backtracking(assignment, cell_to_squares_dict, possible_ints, square_to_cells_dict)
+def backtracking_search(initial, cell_to_groups_dict, possible_ints):    
+   return recursive_backtracking(list(initial), cell_to_groups_dict, possible_ints)
 
-def recursive_backtracking(assignment, tri_to_hex_dict, possible_ints, hex_to_tri_dict):
-   if check_complete(assignment, tri_to_hex_dict, possible_ints, hex_to_tri_dict):
+def recursive_backtracking(assignment, cell_to_groups_dict, possible_ints):
+   if check_complete(assignment):
       s = ""
       for i in assignment:
          s += str(i)
       return s
-   cell = select_unassigned_var(assignment, tri_to_hex_dict, possible_ints, hex_to_tri_dict) 
+   cell = select_unassigned_var(assignment) 
    if cell != None:
-      for value in possible_ints[cell]: # possible_ints maps traingle to [1 - 6]
+      for value in possible_ints[cell]: # possible_ints maps cell to [0 - 9]
          assignment[cell] = int(value)
-         if isValid(value, cell, assignment, tri_to_hex_dict, possible_ints, hex_to_tri_dict):
-            result = recursive_backtracking(assignment, tri_to_hex_dict, possible_ints, hex_to_tri_dict)
-            if check_complete(result, tri_to_hex_dict, possible_ints, hex_to_tri_dict):
+         if isValid(value, cell, assignment, cell_to_groups_dict, possible_ints):
+            result = recursive_backtracking(assignment, cell_to_groups_dict, possible_ints)
+            if check_complete(result):
                return result
             assignment.pop(cell)
             assignment.append(".")
    return None
 
 def display(solution):
-   solution = ''.join([str(i) for i in solution])
    for i in range(0, 81, 9):
       print(solution[i : i + 3], " ", solution[i + 3 : i + 6], " ", solution[i + 6 : i + 9])
       if i == 18 or i == 45:
          print("")
  
+def same_square(i, j): 
+   return int(i / 27) == int(j / 27) and (i % 9) / 3 == (j % 9) / 3
+
 def main(): 
-   
-   cell_to_squares_dict, possible_ints, square_to_cells_dict  = {}, [[] for i in range(24)], {} # cell_to_squares_dict: regions, possible_ints: variables, square_to_cells_dict: adjacents
-
-   cell_to_squares_dict[0] = [1]
-   cell_to_squares_dict[1] = [1]
-   cell_to_squares_dict[2] = [1, 2]
-   cell_to_squares_dict[3] = [2]
-   cell_to_squares_dict[4] = [2]
-   cell_to_squares_dict[5] = [6]
-   cell_to_squares_dict[6] = [1, 6]
-   cell_to_squares_dict[7] = [0, 1, 6]
-   cell_to_squares_dict[8] = [0, 1, 2]
-   cell_to_squares_dict[9] = [0, 2, 3]
-   cell_to_squares_dict[10] = [2, 3]
-   cell_to_squares_dict[11] = [3]
-   cell_to_squares_dict[12] = [6]
-   cell_to_squares_dict[13] = [5, 6]
-   cell_to_squares_dict[14] = [0, 5, 6]
-   cell_to_squares_dict[15] = [0, 4, 5]
-   cell_to_squares_dict[16] = [0, 3, 4]
-   cell_to_squares_dict[17] = [3, 4]
-   cell_to_squares_dict[18] = [3]
-   cell_to_squares_dict[19] = [5]
-   cell_to_squares_dict[20] = [5]
-   cell_to_squares_dict[21] = [4, 5]
-   cell_to_squares_dict[22] = [4]
-   cell_to_squares_dict[23] = [4]
-    
-   for r in range(len(cell_to_squares_dict)): 
-      possible_ints[r] = [i for i in range(10)] # 0 - 9
-
-   square_to_cells_dict[0] = [7, 8, 9, 14, 15, 16]
-   square_to_cells_dict[1] = [0, 1 , 2, 6, 7, 8]
-   square_to_cells_dict[2] = [2, 3, 4, 8, 9, 10]
-   square_to_cells_dict[3] = [9, 10, 11, 16, 17, 18]
-   square_to_cells_dict[4] = [15, 16, 17, 21, 22, 23]
-   square_to_cells_dict[5] = [13, 14, 15, 19, 20, 21]
-   square_to_cells_dict[6] = [5, 6, 7, 12, 13, 14]
-   
    initial = list(sys.argv[1])
+   initial = ''.join([str(i) for i in initial])
+
+   possible_ints = [[] for i in range(81)]
+   for r in range(len(initial)): 
+      possible_ints[r] = [i for i in range(1, 10)] # 1 - 9
+   
+   cell_to_groups_dict = {}
+
+   for cell in range(len(initial)):
+      row = int(cell / 9)
+      col = cell % 9
+      cell_to_groups_dict[cell] = [row, col]
+      
    print("input state", "\n", "-------------------")
+
    display(initial)
+
    print("-------------------", "\n", "solution", "\n", "-------------------")
-   solution = backtracking_search(initial, cell_to_squares_dict, possible_ints, square_to_cells_dict) 
+
+   solution = backtracking_search(initial, cell_to_groups_dict, possible_ints) 
+   solution = ''.join([str(i) for i in solution])
+
    display(solution)
 
 if __name__ == '__main__':
