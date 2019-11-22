1ccb557467a7a9804cb68e4e808bd23334c4665c
Arul Nigam <14031728+ArulNigam@users.noreply.github.com>
2019-11-21 23:40:22 -0500

    close enough...goal is 10 secs for N=64

--- Unit2_ConstraintSatisfactionProblemTechniques/Nigam_A_Lab5.py
+++ Unit2_ConstraintSatisfactionProblemTechniques/Nigam_A_Lab5.py
@@ -1,54 +1,79 @@
 # Name: Arul Nigam
 # Period: 3
 
-import time
+import time, copy
 
 def main():
-   #n = int(input("What is N? "))
-   n = 8
-   board = [[0 for i in range(n)] for j in range(n)]
-   #1 What data structure is used for the board? What does the initial board look like?
-   cur_time = time.time()
-   solution = backtracking_search(board, 0) #2 What does '0' represent?
-   display(solution)
-   print (time.time() - cur_time)
+    n = int(input("What is N? "))
+    columns_done = []
+    board = [[0 for i in range(n)] for j in range(n)]
+    # 1 What data structure is used for the board? What does the initial board look like?
+    cur_time = time.time()
+    solution = backtracking_search(board, columns_done)  # 2 What does '0' represent?
+    display(solution)
+    print(time.time() - cur_time)
 
-def backtracking_search(board, col):
-   if check_complete(board, col): return board
-   for row in range(len(board)):
-      if isValid(row, col, board):
-         board[row][col] = 1
-         result = backtracking_search(board, col + 1) #3 What is the next col?
-         if result != None: return result
-         board[row][col] = 0 #4 In order to backtrack, what code should be here?
-   return None
-   
-def check_complete(board, colNum): #5 Fill out the blank below
-   return colNum == len(board)
-   
+def backtracking_search(board, columns_done):
+    if check_complete(board, 0): return board
+    col = select_unassigned_var(board, columns_done)
+    columns_done.append(col)
+    for row in range(len(board)):
+        if isValid(row, col, board):
+            board[row][col] = 1
+            result = backtracking_search(board, columns_done)  # 3 What is the next col?
+            if result != None: return result
+            board[row][col] = 0  # 4 In order to backtrack, what code should be here?
+    columns_done.remove(col)
+    return None
+
+def check_complete(board, colNum):  # 5 Fill out the blank below
+    lngth = len(board)
+    count = 0
+    for row in range(lngth):
+        for col in range(lngth):
+            if board[row][col] == 1:
+                count += 1
+    return count == lngth
+
 def isValid(row, col, board):
-   for x in range(col):
-      if board[row][x] == 1:
-         return False
-   for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-      if board[i][j] == 1:
-         return False
-   for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
-      if board[i][j] == 1:
-         return False
-   return True
-   
-def select_unassigned_var(assignment):
-    # Select an unassigned variable - forward checking, MRV, or LCV
-    # returns a variable
-    for key in range(81):
-        if assignment[key] == ".":
-            return key
-            
+    for x in range(len(board)):
+        if board[row][x] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, len(board), 1), range(col, len(board), 1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, len(board), 1)):
+        if board[i][j] == 1:
+            return False
+    for x in range(len(board)):
+        if board[x][col] == 1:
+            return False
+    return True
+
+def select_unassigned_var(board, columns_done):  # MRV
+    mincol = 0
+    mincolvalue = 10000
+    for col in range(len(board)):
+        if col not in columns_done:
+            rowcount = 0
+            for row in range(len(board)):
+                if (isValid(row, col, board)):
+                    rowcount += 1
+            if rowcount < mincolvalue and rowcount > 0:
+                mincol = col
+                mincolvalue = rowcount
+    return mincol
+
 def display(solution):
-   for row in solution:
-      print(' '.join([str(i) for i in row]))
-   print("")
-            
+    for row in solution:
+        print(' '.join([str(i) for i in row]))
+    print("")
+
 if __name__ == '__main__':
     main()
