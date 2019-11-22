ecc281fd24df4255d9b79cf7d154956ffab6c66d
Arul Nigam <14031728+ArulNigam@users.noreply.github.com>
2019-11-21 20:14:56 -0500

    default from ms. kim

--- Unit2_ConstraintSatisfactionProblemTechniques/Nigam_A_Lab5.py
+++ Unit2_ConstraintSatisfactionProblemTechniques/Nigam_A_Lab5.py
@@ -5,7 +5,8 @@ import time
 
 def main():
    #n = int(input("What is N? "))
-   board = [[0 for i in range(8)] for j in range(8)]
+   n = 8
+   board = [[0 for i in range(n)] for j in range(n)]
    #1 What data structure is used for the board? What does the initial board look like?
    cur_time = time.time()
    solution = backtracking_search(board, 0) #2 What does '0' represent?
@@ -17,21 +18,14 @@ def backtracking_search(board, col):
    for row in range(len(board)):
       if isValid(row, col, board):
          board[row][col] = 1
-         result = backtracking_search(board,________) #3 What is the next col?
+         result = backtracking_search(board, col + 1) #3 What is the next col?
          if result != None: return result
-         _________________________ #4 In order to backtrack, what code should be here?
+         board[row][col] = 0 #4 In order to backtrack, what code should be here?
    return None
    
 def check_complete(board, colNum): #5 Fill out the blank below
-   _____________________________________________________________
-   if assignment is None or assignment == []:
-       return False
-   for cell_value in assignment:
-       if cell_value == ".":
-           return False
-   return True
-
-
+   return colNum == len(board)
+   
 def isValid(row, col, board):
    for x in range(col):
       if board[row][x] == 1:
@@ -43,3 +37,18 @@ def isValid(row, col, board):
       if board[i][j] == 1:
          return False
    return True
+   
+def select_unassigned_var(assignment):
+    # Select an unassigned variable - forward checking, MRV, or LCV
+    # returns a variable
+    for key in range(81):
+        if assignment[key] == ".":
+            return key
+            
+def display(solution):
+   for row in solution:
+      print(' '.join([str(i) for i in row]))
+   print("")
+            
+if __name__ == '__main__':
+    main()
