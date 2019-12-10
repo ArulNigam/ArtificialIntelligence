21c57c09dfcdf9377db5c7819c60725635ac6a04
arul nigam <14031728+ArulNigam@users.noreply.github.com>
2019-12-09 22:33:46 -0500

    fixed
    mrs. kim's code had problems

--- Unit3_AdversarialSearch/Nigam_A_Lab1.py
+++ Unit3_AdversarialSearch/Nigam_A_Lab1.py
@@ -1,6 +1,7 @@
 # Name: Arul Nigam
 # Date: 12/05/2019
 # Change the file name to lastName_firstInitial_Lab1.py
+import time
 
 def terminal_test(state, tc):
     empty_spot = state.find('.')
@@ -15,40 +16,58 @@ def utility(turn, tc, state):
     # return 1 (turn wins), -1 (turn loses), or 0 (tie)
     for line in tc:
         if state[line[0]] == state[line[1]] and state[line[1]] == state[line[2]]:
-            if state[line[0]] == turn:
+            if state[line[0]] == "X":
                 return 1
-            elif state[line[0]] in ['O', 'X']:
+            elif state[line[0]] == "O":
                 return -1
     return 0
 
 def minimax(state, turn, tc, goal_turn):
-   if turn == goal_turn:
-      v, s = max_value(state, turn, tc)
-   else:
-      v, s = min_value(state, turn, tc)
-   return s
+    if turn == "X":
+        v, s = max_value(state, turn, tc)
+    else:
+        v, s = min_value(state, turn, tc)
+    return s
 
 def max_value(state, turn, tc):
     if terminal_test(state, tc):
         return utility(turn, tc, state)
     v = -9999
+    if turn == "X":
+        newturn = "O"
+    else:
+        newturn = "X"
     for s in successors(state, turn):
-        v = max(v, min_value(s, turn, tc))
-    return v, s
+        newv = min_value(s, newturn, tc)
+        if not (isinstance(newv, int)):
+            newv = newv[0]
+        if v < newv:
+            v = newv
+            res = s
+    return v, res
 
 def min_value(state, turn, tc):
     if terminal_test(state, tc):
         return utility(turn, tc, state)
     v = 9999
+    if turn == "X":
+        newturn = "O"
+    else:
+        newturn = "X"
     for s in successors(state, turn):
-        v = min(v, max_value(s, turn, tc))
-    return v, s
+        newv = max_value(s, newturn, tc)
+        if not (isinstance(newv, int)):
+            newv = newv[0]
+        if v > newv:
+            v = newv
+            res = s
+    return v, res
 
 def successors(state, turn):
     ret = []
     for i in range(len(state)):
         if state[i] == ".":
-            ret.append(state[ : i] + turn + state[i + 1 : ])
+            ret.append(state[: i] + turn + state[i + 1:])
     return ret
 
 def get_turn(state):
@@ -84,45 +103,46 @@ def display(state, n=3, n2=9):
 def human_play(s, n, turn):
     index_li = [x for x in range(len(s)) if s[x] == '.']
     for i in index_li:
-        print ('[%s] (%s, %s)' % (i, i // n, i % n))
+        print('[%s] (%s, %s)' % (i, i // n, i % n))
     index = input("What's your input?")
     state = s[0:int(index)] + turn + s[int(index) + 1:]
     return state
 
 def main():
-    X = raw_input("X is human or AI? (h: human, a: AI)")
-    O = raw_input("O is human or AI? (h: human, a: AI)")
-    state = '' + raw_input("input state (ENTER if it's an empty state): ")
+    X = input("X is human or AI? (h: human, a: AI)")
+    O = input("O is human or AI? (h: human, a: AI)")
+    state = '' + input("input state (ENTER if it's an empty state): ")
     if len(state) == 0: state = '.........'
     turn = get_turn(state)
     tc = conditions_table(3, 9)
-    print ("Game start!")
-    print (display(state, 3, 9))
+    print("Game start!")
+    print(display(state, 3, 9))
     while terminal_test(state, tc) == False:
         if turn == 'X':
-            print ("{}'s turn:".format(turn))
+            print("{}'s turn:".format(turn))
             if X == 'a':
                 state = minimax(state, turn, tc, 'X')
             else:
                 state = human_play(state, 3, turn)
-            print (display(state, 3, 9))
+            print(display(state, 3, 9))
             turn = 'O'
         else:
-            print ("{}'s turn:".format(turn))
+            print("{}'s turn:".format(turn))
             if O == 'a':
                 state = minimax(state, turn, tc, 'O')
             else:
                 state = human_play(state, 3, turn)
-            print (display(state, 3, 9))
+            print(display(state, 3, 9))
             turn = 'X'
     if utility(turn, tc, state) == 0:
-        print ("Game over! Tie!")
+        print("Game over! Tie!")
     else:
         if turn == 'O':
             turn = 'X'
         else:
             turn = 'O'
-        print ('Game over! ' + turn + ' win!')
+        print('Game over! ' + turn + ' win!')
+
 
 if __name__ == '__main__':
     main()
