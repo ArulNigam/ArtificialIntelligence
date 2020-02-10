a5bbee5aa2b008f1889a7837a090b590d585e584
arul nigam <14031728+ArulNigam@users.noreply.github.com>
2020-02-10 14:06:42 -0500

    Update Nigam_A_Lab5.py

--- Unit4_RegEx/Nigam_A_Lab5.py
+++ Unit4_RegEx/Nigam_A_Lab5.py
@@ -8,82 +8,79 @@ BLOCKCHAR = '#'
 OPENCHAR = "-"
 PROTECTEDCHAR = "'~"
 
-
 class Crossword():
-    def init():  #####
-        board = ""
-        row_max
-        col_max
-        length = row_max * col_max
+    def __init__(self):
+        self.board = ""
+        self.row_max = 1 ##########################
+        self.col_max = 1 ##########################
+        self.length = self.row_max * self.col_max
 
     def add_blocked_squares():
 
-    def check_connectivity(board):
-        
+    def check_connectivity(self, board):
         start = board.index(OPENCHAR)
         if start < 0:
             start = board.index(PROTECTEDCHAR)
-        flood_filled = connectivity_helper(start, board, [], [])
+        flood_filled = self.connectivity_helper(start, board, [], [])
+        return len(flood_filled) == (board.count(OPENCHAR) + board.count(PROTECTEDCHAR))
 
-    def adjacents(space, explored):
+    def adjacents(self, space, explored):
         ret = []
-        temp = [space + 1, space - 1, space + row_max, space - row_max]
+        temp = [space + 1, space - 1, space + self.row_max, space - self.row_max]
         for i in temp:
             if i not in explored:
                 ret.append(i)
         return ret
 
-    def connectivity_helper(space, board, connected, explored):
+    def connectivity_helper(self, space, board, connected, explored):
         if (board[space] == OPENCHAR) or (board[space] == PROTECTEDCHAR):
             connected.append(space)
-            for temp_space in adjacents(space):
-                connected.append(connectivity_helper(temp_space, board, connected))
+            for temp_space in self.adjacents(space):
+                connected.append(self.connectivity_helper(temp_space, board, connected))
             return connected
 
     def index_to_coordinates(index):
 
-    def check_legal(moved_board):
-        return make_palindrome(moved_board)[1]
+    def check_legal(self, moved_board):
+        return self.make_palindrome(moved_board)[1]
 
-    def make_palindrome(temp_board):  # check if it properly captures middle
+    def make_palindrome(self, temp_board):  # check if it properly captures middle
         board_list = list(temp_board):
         for i in range(len(board_list)):
-            if board_list[i] != board_list[length - i]:
-                if board_list[i] == PROTECTEDCHAR or board_list[length - i] == PROTECTEDCHAR:
-                    if board_list[i] == OPENCHAR or board_list[length - i] == OPENCHAR:
+            if board_list[i] != board_list[self.length - i]:
+                if board_list[i] == PROTECTEDCHAR or board_list[self.length - i] == PROTECTEDCHAR:
+                    if board_list[i] == OPENCHAR or board_list[self.length - i] == OPENCHAR:
                         board_list[i] = PROTECTEDCHAR
-                        board_list[length - i] = PROTECTEDCHAR
-                    if board_list[i] == BLOCKCHAR or board_list[length - i] == BLOCKCHAR:
+                        board_list[self.length - i] = PROTECTEDCHAR
+                    if board_list[i] == BLOCKCHAR or board_list[self.length - i] == BLOCKCHAR:
                         return ["", False]  # THERE IS A BOARD CONFLICT
                 else:
                     board_list[i] = BLOCKCHAR
-                    board_list[length - i] = BLOCKCHAR
+                    board_list[self.length - i] = BLOCKCHAR
         return [''.join(board_list), True]
 
-    def coordinates_to_index(row_num, col_num):
-        return (row_num * col_max + col_num)
+    def coordinates_to_index(self, row_num, col_num):
+        return row_num * self.col_max + col_num
 
-    def display(temp_board):
-        clean_board = clean_protected(temp_board)
-        for row in range(row_max):
-            print(clean_board[(row * col_max):(row * col_max + col_max)])
+    def display(self, temp_board):
+        clean_board = self.clean_protected(temp_board)
+        for row in range(self.row_max):
+            print(clean_board[(row * self.col_max):(row * self.col_max + self.col_max)])
             print()
 
-    def clean_protected(temp_board):
+    def clean_protected(self, temp_board):
         board_list = list(temp_board)
         for i in board_list:
             if board_list[i] == PROTECTEDCHAR:
                 board_list[i] = OPENCHAR
         return ''.join(board_list)
 
-
 ####################################################################################
 
 def main():
     input = sys.argv
     temp = Crossword()
-    display(board)
-
+    #display(board)
 
 if __name__ == '__main__':
     main()
